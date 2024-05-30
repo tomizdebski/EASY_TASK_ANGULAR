3abe5a39ddc7c8d2904e6f901578cca69b4ea135
tomizdebski <tomizdebski@gmail.com>
2024-05-30 13:40:36 +0200

    features/add-form-add-task

---
+++ src/app/tasks/new-task/new-task.component.ts
@@ -0,0 +1,15 @@
+import { Component, EventEmitter, Output } from '@angular/core';
+
+@Component({
+  selector: 'app-new-task',
+  standalone: true,
+  imports: [],
+  templateUrl: './new-task.component.html',
+  styleUrl: './new-task.component.css'
+})
+export class NewTaskComponent {
+  @Output() closeDialog = new EventEmitter<void>();
+  onCloseDialog() {
+    this.closeDialog.emit();
+  }
+}
--- src/app/tasks/tasks.component.ts
+++ src/app/tasks/tasks.component.ts
@@ -1,16 +1,20 @@
 import { Component, Input } from "@angular/core";
 import { TaskComponent } from "./task/task.component";
+import { Task } from "./task/task.model";
+import { NewTaskComponent } from "./new-task/new-task.component";
 
 @Component({
-  selector: "app-tasks",
-  standalone: true,
-  imports: [TaskComponent],
-  templateUrl: "./tasks.component.html",
-  styleUrl: "./tasks.component.css",
+    selector: "app-tasks",
+    standalone: true,
+    templateUrl: "./tasks.component.html",
+    styleUrl: "./tasks.component.css",
+    imports: [TaskComponent, NewTaskComponent]
 })
 export class TasksComponent {
   @Input({ required: true }) userId!: string;
   @Input({ required: true }) name!: string;
+  isAddingTask = false;
+
   tasks = [
     {
       id: "t1",
@@ -41,5 +45,12 @@ export class TasksComponent {
   onCompleteTask(taskId: string) {
     this.tasks = this.tasks.filter((task) => task.id !== taskId);
 
+  }
+
+  onStartAddTask() {
+    this.isAddingTask = true;
+  }
+  onCancelAddTask() {
+    this.isAddingTask = false;
   }
 }
